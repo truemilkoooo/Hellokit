94b5cf233f17b191ef62dd03ae91503675b4c480
truemilkoooo <truemilkoooo@gmail.com>
2021-10-01 23:52:47 +0300

    Initial Commit

---
+++ Hellokit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hellokit
+//
+//  Created by Вова Пупкин on 01.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
